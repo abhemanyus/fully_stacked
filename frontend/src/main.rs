c899d5e7f2fcdf663f8889a01c303186c97ab50a
Abhemanyu Sarin <abhemanyus@gmail.com>
2022-07-30 10:26:09 +0530

    skeleton complete

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,17 @@
+use seed::{prelude::*, *};
+
+enum Msg {}
+struct Model {}
+
+fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
+    Model {}
+}
+fn update(_: Msg, _: &mut Model, _: &mut impl Orders<Msg>) {}
+
+fn view(_: &Model) -> Node<Msg> {
+    div!["Hello, World!"]
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::start("app", init, update, view);
 }
